681a5468b50398193b98427584ef9d7d5d3821ba
zty <782094766@qq.com>
2020-10-14 19:14:52 +0800

    1.0

---
+++ js/main.js
@@ -0,0 +1,43 @@
+;
+(function (doc) {
+    var oMenuBtn = doc.querySelector('.header .fa-bars');
+    var oNav = doc.querySelector('.header .header-nav');
+
+
+    var init = function () {
+       setFontSize();
+       bindEvent();
+    }
+
+    function bindEvent(){
+        window.addEventListener('resize',setFontSize,false);
+        oMenuBtn.addEventListener('click',showNav,false);
+    }
+
+    function setFontSize(){
+        var cWidth = doc.documentElement.clientWidth;
+        if (cWidth <= 414) {
+            doc.documentElement.style.fontSize = cWidth/37.5 + 'px';
+        }else{
+            const fontSize = doc.documentElement.style.fontSize;
+            if(fontSize != '62.5%'){
+                doc.documentElement.style.fontSize = '62.5%';
+            }
+        }
+        if(cWidth>768){
+            oNav.style.display = 'block'
+        }else{
+            oNav.style.display = 'none'
+        }
+    }
+
+    function showNav(){
+        var isShow = oNav.style.display;
+        if(isShow==='block'){
+            oNav.style.display = 'none'
+        }else{
+            oNav.style.display = 'block'
+        }
+    }
+    init();
+})(document);
